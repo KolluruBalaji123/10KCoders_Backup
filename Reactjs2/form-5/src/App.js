870a308e44c48815dad30c4efaaea7c552525906
--global <kollurubalaji7@gmail.com>
2022-12-15 14:01:38 +0530

    Form4

---
+++ Reactjs2/form-5/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Registration from './Components/Registration';
+
+
+
+function App() {
+  return (
+    <div className="App">
+    <Registration/>
+
+  
+    
+    </div>
+  );
+}
+
+export default App;
