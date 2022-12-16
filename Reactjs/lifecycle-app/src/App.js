79cd41985e8305b4cddbbd0169d58dd1de1cea1e
kolluruBalaji123 <kollurubalaji7@gmail.com>
2022-12-17 03:07:40 +0530

    checks

--- Reactjs/lifecycle-app/src/App.js
+++ Reactjs/lifecycle-app/src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import LifeCycleA from './Comp/Lifea';
+import PersonDetails from './Frmsbrn-check/check';
 
 
 function App() {
   return (
     <div className="App">
-    <LifeCycleA/>
+    {/* <LifeCycleA/> */}
+    <PersonDetails/>
     
     </div>
   );
