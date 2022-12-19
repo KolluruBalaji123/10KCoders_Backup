276515f4030725f86f483c52e59e4ae766d21208
kolluruBalaji123 <kollurubalaji7@gmail.com>
2022-12-19 16:55:53 +0530

    5 formsDone

---
+++ LifeCycleFetch/my-form5/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Myform5 from './Components/myform5';
+
+function App() {
+  return (
+    <div className="App">
+    <Myform5/>
+    
+    </div>
+  );
+}
+
+export default App;
